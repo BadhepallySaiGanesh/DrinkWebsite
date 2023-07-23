37a9fd19956770fd7fe7510d675c2bdc52c388e7
BadhepallySaiGanesh <bganeshgoud007@gmail.com>
2023-07-23 14:54:15 +0530

    okay

--- drinkwebsite/src/Drink.jsx
+++ drinkwebsite/src/Drink.jsx
@@ -1,42 +1,122 @@
-import React, { useState , useEffect } from 'react'
+// import React, { useState , useEffect } from 'react'
 
-// const URL ="www.thecocktaildb.com/api/json/v1/1/search.php?s=o";
-const URL ="www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+// // const URL ="www.thecocktaildb.com/api/json/v1/1/search.php?s=o";
+// const URL ="www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
-const Drink = () => {
-    const [drinkdata,SetDrinkData] = useState([]);
+// const Drink = () => {
+//     const [drinkdata,SetDrinkData] = useState([]);
 
-    const fetchDrink = async(apiURL) => {
-        const response = await fetch(apiURL);
-        const {drink} = await response.json();
-        console.log(drink);
-        SetDrinkData(drink);
-    };
+//     const fetchDrink = async(apiURL) => {
+//         const response = await fetch(apiURL);
+//         const {drink} = await response.json();
+//         console.log(drink);
+//         SetDrinkData(drink);
+//     };
 
-    useEffect(()=> {
-        fetchDrink(URL);
-    },[]);
+//     useEffect(()=> {
+//         fetchDrink(URL);
+//     },[]);
+
+//   return (
+//     <div>
+//         <form>
+//           <input type="text" className="search"  name='search' id='search' placeholder='search something new'/>
+//         </form>
+//         <hr/>
+//         <ul>
+//           {
+//             drinkdata.map((eachDrink)=>{
+//               const {idDink,strDrink,} = eachDrink;
+//               return <li>
+//                 <div>
+//                   <img src="" alt="" />
+//                 </div>
+//               </li>
+//             })
+//           }
+//         </ul>
+//     </div>
+//   );
+// };
+
+// export default Drink;
+
+
+import React, { useState, useEffect } from "react";
+
+const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+const Final = () => {
+  const [drinksData, setDrinksData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState({ status: false, msg: "" });
+
+  const fetchDrink = async (apiURL) => {
+    setLoading(true);
+    setIsError({ status: false, msg: "" });
+    try {
+      const response = await fetch(apiURL);
+      const { drinks } = await response.json();
+      setDrinksData(drinks);
+      setLoading(false);
+      setIsError({ status: false, msg: "" });
+      if (!drinks) {
+        throw new Error("Data Not Found Because U searched Worng");
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setIsError({
+        status: true,
+        msg: error.message || "something went wrong...",
+      });
+    }
+  };
+
+  useEffect(() => {
+    const correctURL = `${URL}${searchTerm}`;
+    fetchDrink(correctURL);
+  }, [searchTerm]);
 
   return (
     <div>
-        <form>
-          <input type="text" className="search"  name='search' id='search' placeholder='search something new'/>
-        </form>
-        <hr/>
-        <ul>
-          {
-            drinkdata.map((eachDrink)=>{
-              const {idDink,strDrink,} = eachDrink;
-              return <li>
+      <h1>Soft Drink</h1>
+      <form className="searchbar">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="search Drinks"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </form>
+      <hr className="hr" / >
+      {loading && !isError?.status && <h3>Loading...</h3>}
+      {isError?.status && <h2 style={{ color: "red",textAlign:"center"}}>{isError.msg}</h2>}
+      {!loading && !isError?.status && (
+        <ul className="cocktaildata">
+          {drinksData.map((eachDrink) => {
+            const { idDrink, strDrink, strDrinkThumb,strCategory,strAlcoholic, } = eachDrink;
+            return (
+              <li key={idDrink}>
                 <div>
-                  <img src="" alt="" />
+                  <img src={strDrinkThumb} alt={strDrink} />
                 </div>
+                <div className="text">
+                  <h3>DrinkName:{strDrink}</h3>
+                  <h3>Category:{strCategory}</h3>
+                <span>{strAlcoholic} </span>
+                </div>
+                <hr/>
               </li>
-            })
-          }
+            );
+          })}
         </ul>
+      )}
     </div>
   );
 };
 
-export default Drink;
+export default Final;
